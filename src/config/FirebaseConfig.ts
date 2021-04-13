8a636eb12e1da8deb11047f2ea07558c94ae56eb
Oliver Dietsche <oliver.dietsche@namics.com>
2021-04-13 15:53:26 +0200

    fix: remove sensitive values from repository

--- src/config/FirebaseConfig.ts
+++ src/config/FirebaseConfig.ts
@@ -1,8 +1,8 @@
-export const FirebaseConfig: IFirebaseConfig = {
-	apiKey: 'AIzaSyCBv9aaCtOBoBq7VQtvbRVRadJ_4c5QTU4',
-	authDomain: 'workshops-ipa.firebaseapp.com',
-	projectId: 'workshops-ipa',
-	storageBucket: 'workshops-ipa.appspot.com',
-	messagingSenderId: '523771032079',
-	appId: '1:523771032079:web:6b06cc11a1d7292c36e97a',
+export const FirebaseConfig: Partial<IFirebaseConfig> = {
+	apiKey: process.env.apiKey,
+	authDomain: process.env.authDomain,
+	projectId: process.env.projectId,
+	storageBucket: process.env.storageBucket,
+	messagingSenderId: process.env.messagingSenderId,
+	appId: process.env.appId,
 };
